f07f7d1f10f0d77c8efd8d5d03dad8c2a481a05c
Mehran Ziabary <ziabary@targoman.com>
2014-12-03 07:54:30 +0000

    Base SMTCore comited

---
+++ SMTCore/test/main.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+ * Copyright © 2012-2014, Targoman.com
+ *
+ * Published under the terms of TCRL(Targoman Community Research License)
+ * You can find a copy of the license file with distributed source or
+ * download it from http://targoman.com/License.txt
+ *
+ *************************************************************************/
+/**
+ @author S. Mohammad M. Ziabary <smm@ziabary.com>
+ */
+
+#include "libLIB_NAME/???.h"
+
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+}
+
+
