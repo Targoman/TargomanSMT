d175a5547b692ab6e53f6cb67aa8d4b85d5d6acf
Mehran Ziabary <ziabary@targoman.com>
2015-02-02 11:44:32 +0000

    missing files added

---
+++ CommonLibs/libsrc/libTargomanCommon/CompressedStream/clsCompressedInputStream.cpp
@@ -0,0 +1,56 @@
+/*************************************************************************
+ * Copyright © 2012-2015, Targoman.com
+ *
+ * Published under the terms of TCRL(Targoman Community Research License)
+ * You can find a copy of the license file with distributed source or
+ * download it from http://targoman.com/License.txt
+ *
+ *************************************************************************/
+/**
+ @author S. Mohammad M. Ziabary <smm@ziabary.com>
+ @author Behrooz Vedadian <vedadian@gmail.com>
+ */
+
+#include "clsCompressedInputStream.h"
+#include "clsCompressedStreamBuff.h"
+#include "../exTargomanBase.h"
+
+namespace Targoman {
+namespace Common {
+namespace CompressedStream {
+
+clsCompressedInputStream::clsCompressedInputStream() : std::istream(0), Buffer(0) {}
+
+clsCompressedInputStream::clsCompressedInputStream(const std::string &_name, bool _allowFail):
+    std::istream(0), Buffer(0)
+{
+    open(_name, _allowFail);
+}
+
+void clsCompressedInputStream::open(const std::string &_name, bool _allowFail)
+{
+    if (this->Buffer) delete this->Buffer;
+    if (_name == "-") {
+        this->Buffer = std::cin.rdbuf();
+    } else {
+        clsCompressedStreamBuff *CompressedBuffer = new clsCompressedStreamBuff();
+        if (!CompressedBuffer->open(_name, std::ios::in)) {
+            if (!_allowFail)
+                throw exTargomanBase("Could not open \"" + QString::fromStdString(_name) + "\" for reading");
+            setstate(std::ios::failbit);
+        }
+        this->Buffer = CompressedBuffer;
+    }
+    this->rdbuf(this->Buffer);
+}
+void clsCompressedInputStream::close()
+{
+    if (this->Buffer) {
+        if (this->Buffer != std::cin.rdbuf()) delete this->Buffer;
+        this->Buffer = 0;
+        rdbuf(0);
+    }
+}
+}
+}
+}
---
+++ CommonLibs/libsrc/libTargomanCommon/CompressedStream/clsCompressedInputStream.h
@@ -0,0 +1,56 @@
+/*************************************************************************
+ * Copyright © 2012-2015, Targoman.com
+ *
+ * Published under the terms of TCRL(Targoman Community Research License)
+ * You can find a copy of the license file with distributed source or
+ * download it from http://targoman.com/License.txt
+ *
+ *************************************************************************/
+/**
+ @author S. Mohammad M. Ziabary <smm@ziabary.com>
+ @author Behrooz Vedadian <vedadian@gmail.com>
+ */
+
+#ifndef TARGOMAN_COMMON_COMPRESSEDSTREAM_CLSCOMPRESSEDINPUTSTREAM_H
+#define TARGOMAN_COMMON_COMPRESSEDSTREAM_CLSCOMPRESSEDINPUTSTREAM_H
+
+// standard C++ with new header file names and std:: namespace
+#include <iostream>
+#include <fstream>
+
+namespace Targoman {
+namespace Common {
+namespace CompressedStream {
+
+// ----------------------------------------------------------------------------
+// Internal classes to implement CompressedStream. See below for user classes.
+// ----------------------------------------------------------------------------
+
+class clsCompressedStreamBuff;
+
+// ----------------------------------------------------------------------------
+// User classes. Use CompressedInputStream and CompressedOutputStream analogously to ifstream and
+// ofstream respectively. They read and write files based on the gz*
+// function interface of the zlib. Files are compatible with gzip compression.
+// ----------------------------------------------------------------------------
+
+class clsCompressedInputStream : public std::istream
+{
+public:
+    clsCompressedInputStream();
+    clsCompressedInputStream(const std::string &_name, bool _allowFail=false);
+    ~clsCompressedInputStream() { close(); }
+    void open(const std::string &_name, bool _allowFail=false);
+    void close();
+    bool isOpen() const { return Buffer; }
+
+private:
+std::streambuf *Buffer;
+
+};
+
+}
+}
+}
+
+#endif // TARGOMAN_COMMON_COMPRESSEDSTREAM_CLSCOMPRESSEDINPUTSTREAM_H
